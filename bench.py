64220fdd736c5f384479e98151703e1ce80d75d3
Mhijazi16 <gultimate0@gmail.com>
2024-09-12 16:33:55 +0300

    fixes

--- bench.py
+++ bench.py
@@ -5,18 +5,21 @@ def benchmark(model_name: str):
     llm = ChatOllama(model=model_name, temperature=0)
     start = time.time()
     tokens = []
-    for token in llm.stream("Write a TCP server in C++"): 
-        print(f"[🍿] Tokens Written: {len(tokens)}", flush=True, end="\r")
+    for token in llm.stream("Write a paragraph about AI "): 
+        print(f"🍿 Tokens Written: [{len(tokens)}]", flush=True, end="\r")
         tokens += token.content
     end = time.time()
-    print(f"[⛽] Finished Benchmark For: {model_name}")
-    print(f"[🪖] Total Toekens: {len(tokens)}")
-    print(f"[⏲️] Time Taken: {end-start} seconds")
-    print(f"[🎖️] Token/Seconds: {len(tokens)/(end-start)}")
+    print()
+    print(f"(⛽) Finished Benchmark For: [{model_name}]")
+    print(f"(🪖) Total Toekens: [{len(tokens)}]")
+    print(f"(⏲️) Time Taken: [{end-start}s]")
+    print(f"(🎖️) Token/Seconds: [{len(tokens)/(end-start)}]")
+    print()
 
 
 models = ["llama3.1","llama3-groq-tool-use","internlm2","deepseek-coder-v2","gemma2"]
 for model in models: 
+    print("==========================================")
     print(f"[💺] STARTING BENCHMARK FOR {model}")
     print("==========================================")
     benchmark(model)
