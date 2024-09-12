0b6d86bfbadefb0dbd3c1fe8edd1b901dd473ae1
Mhijazi16 <gultimate0@gmail.com>
2024-09-12 16:07:09 +0300

    writing the number of tokens written

--- bench.py
+++ bench.py
@@ -1,7 +1,15 @@
+from langchain_ollama import ChatOllama
+
 def benchmark(model_name: str): 
-    pass
+    llm = ChatOllama(model=model_name, temperature=0)
+    tokens = []
+    for token in llm.stream("Write a TCP server in C++"): 
+        print(f"🍿Tokens Writtern: {len(tokens)}", flush=True, end="\r")
+        tokens += token.content
+
 
 models = ["llama3.1","llama3-groq-tool-use","internlm2","deepseek-coder-v2","gemma2"]
 for model in models: 
     print(f"[💺] STARTING BENCHMARK FOR {model}")
     benchmark(model)
+
